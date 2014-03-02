164f30ffd6be278651f851a037e150dfd2ef9814
Leandro Pisaroni <lpisaroni@gmail.com>
2014-03-02 04:44:06 -0300

    paper click

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,5 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
+
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://www.nndb.com/people/757/000022691/jim1-80.jpg"
 end
